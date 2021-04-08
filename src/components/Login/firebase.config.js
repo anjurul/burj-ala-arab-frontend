10eacb5b34697d9a6a60a81f58b6ad286f6407e6
anjurul <anjurul.ewallet@gmail.com>
2021-04-09 00:06:59 +0400

    burj-al-arab-frontend

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAWMJ7spNlqNmZ4obfRuBGLI4mwPnS0ydk",
+  authDomain: "burj-al-arab-simple-auth.firebaseapp.com",
+  projectId: "burj-al-arab-simple-auth",
+  storageBucket: "burj-al-arab-simple-auth.appspot.com",
+  messagingSenderId: "117965564422",
+  appId: "1:117965564422:web:5f578f68fec37208b03187"
+};
+
+export default firebaseConfig;
